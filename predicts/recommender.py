9f7fe1c9e6a3a7df517fd792c607b104cdba9c31
thiagomedinad <thiagomedinad21@gmail.com>
2023-11-28 14:26:55 -0300

    changes

--- predicts/recommender.py
+++ predicts/recommender.py
@@ -11,20 +11,16 @@ class MovieRecommender:
         self.best_movie = {}
 
     def vectorize(self):
-        # Initialize the vectorizer and transform the 'combined_features' to a matrix
         tfidf = TfidfVectorizer(stop_words='english')
         self.tfidf_matrix = tfidf.fit_transform(self.df['combined_features'])
 
     def calculate_similarity(self):
-        # Calculate cosine similarity matrix
         self.cosine_sim = cosine_similarity(self.tfidf_matrix, self.tfidf_matrix)
 
     def build_indices(self):
-        # Create a reverse map of indices and movie titles
         self.indices = pd.Series(self.df.index, index=self.df['Series_Title']).drop_duplicates()
 
     def recommend(self, title, num_recommendations=10):
-        # Get recommendations for a movie title
         idx = self.indices[title]
         sim_scores = list(enumerate(self.cosine_sim[idx]))
         sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
@@ -32,9 +28,6 @@ class MovieRecommender:
         movie_indices = [i[0] for i in sim_scores]
         grades = [i[1] for i in sim_scores]
 
-        # Store the best movie
         self.best_movie[grades[0]] = self.df['Series_Title'].iloc[movie_indices[0]]
 
-        # Return the recommended movies
-        # return self.df['Series_Title'].iloc[movie_indices]
         return self.best_movie[grades[0]]
